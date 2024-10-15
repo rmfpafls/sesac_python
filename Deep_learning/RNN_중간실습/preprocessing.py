d060c1bcf1cc0cfb3045b7c06eb19682761da3f8
hyunji <brown915@naver.com>
2024-10-15 21:42:18 +0900

    1015
    1015 실습

--- Deep_learning/RNN_중간실습/preprocessing.py
+++ Deep_learning/RNN_중간실습/preprocessing.py
@@ -1,9 +1,40 @@
-from load_file import load_file
+import torch
+import random 
+import numpy as np 
+from torch.utils.data import DataLoader, TensorDataset
 from collections import defaultdict
+
+from load_file import load_file
+
+
 
 
 OOV = '[OOV]'
 PAD = '[PAD]'
+
+def letter2tensor(letter, alphabets, oov = OOV):
+    res = [0 for _ in range(len(alphabets))]
+
+    if letter in alphabets:
+        idx = alphabets.index(letter)
+    else:
+        idx = alphabets.index(oov)
+
+    res[idx] = 1
+
+    return torch.tensor(res)
+    
+def word2tensor(word, max_length, alphabets, pad = PAD, oov = OOV):
+    res = torch.zeros(max_length, len(alphabets))
+
+    for idx, char in enumerate(word): 
+        if idx < max_length: 
+            res[idx] = letter2tensor(char, alphabets, oov = oov)
+
+    for i in range(max_length - len(word)): # padding 
+        res[len(word)+i] = letter2tensor(pad, alphabets, oov = OOV)
+    return res
+
 
 def determine_alphabets(data, pad = PAD, oov = OOV, threshold = 0.999):
     lst = []
@@ -11,7 +42,7 @@ def determine_alphabets(data, pad = PAD, oov = OOV, threshold = 0.999):
 
     for name, lang in data: 
         for char in name: 
-            character_dict[char.lower()] += 1
+            character_dict[char.lower()] += 1 # 이거 알파벳이 아닌 글자에는 그대로 나오기 때문에 lower() 가능
     
     for k, v in character_dict.items(): 
         lst.append((k,v))
@@ -51,13 +82,64 @@ def determine_max_length(data, threshole = 0.99):
         if s > threshole * total_count: 
             return k -1 # threshole퍼센트 이상이면 그 전 단어 횟수만큼만 사용하겠다. 
         
-    return max(lst, key = lambda x:x[0]) # 제일 긴 단어 길이 
+    # return max(lst, key = lambda x:x[0]) # 제일 긴 단어 길이 - 이거 사용안하는데 왜 씀 
+
+def split_train_valid_test(x, y, train_size = 0.8, valid_size = 0.1, test_size = 0.1):
     
+    train_max = int(len(x)*train_size)
+
+    train_x = x[:train_max]
+    train_y = y[:train_max]
+
+    valid_max = int(train_max + len(x)*valid_size)
 
 
+    valid_x = x[train_max:valid_max]
+    valid_y = y[train_max:valid_max]
+
+    test_x = x[valid_max:]
+    test_y = y[valid_max:]
+
+    return train_x, train_y, valid_x, valid_y, test_x, test_y 
 
 def generate_dataset(batch_size = 32, pad = PAD, oov = OOV):
     data, languages = load_file()
-    alphabets = determine_alphabets(data, pad = pad, oov = oov)
-    max_length = determine_max_length(data)
-    print(alphabets, max_length) 
+    alphabets = determine_alphabets(data, pad = pad, oov = oov) # 사용된 모든 단어+pad+oov
+    max_length = determine_max_length(data) # 사용할 단어의 최대 길이 
+
+    np.random.shuffle(data) #앞쪽 파일만 학습하는 걸 방지하려고 한번 섞음 
+
+    for idx, elem in enumerate(data): # elem = [name, lang]
+        tmp = []
+
+        for char in elem[0]: 
+            if char.lower() in alphabets: 
+                tmp.append(char.lower())
+            else: 
+                tmp.append(oov) 
+        
+        data[idx][0] = word2tensor(tmp, max_length, alphabets, pad = PAD, oov = OOV)
+        data[idx][1] = languages.index(data[idx][1]) # languages를 인덱스 형태로 바꿈
+
+    x = [e[0] for e in data]
+    y = [torch.tensor(e[1]) for e in data] 
+
+    train_x, train_y, valid_x, valid_y, test_x, test_y = split_train_valid_test(x,y)
+
+    train_x = torch.stack(train_x)
+    train_y = torch.stack(train_y)
+    valid_x = torch.stack(valid_x)
+    valid_y = torch.stack(valid_y)
+    test_x = torch.stack(test_x)
+    test_y = torch.stack(test_y)
+
+    train_dataset = TensorDataset(train_x, train_y)
+    valid_dataset = TensorDataset(valid_x, valid_y)
+    test_dataset = TensorDataset(test_x, test_y)
+
+    train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle= True)
+    valid_dataloader = DataLoader(valid_dataset, batch_size = batch_size, shuffle= True)
+    test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle = True)
+
+    return train_dataset, valid_dataset, test_dataset, alphabets, max_length, languages
+ 
